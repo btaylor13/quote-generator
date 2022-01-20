87999129a7ae9ef0b572aaf4d3130fe84a0703e2
Bridgett Taylor <btaylor7224@gmail.com>
2022-01-19 17:08:13 -0800

    added js functionality to the new quote and share twitter btns

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // pick random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // check if author field is blank and replace it with unknown
+  if (!quote.source) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.source;
+  }
+
+  // Check quote length
+  if (quote.quote.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.quote;
+}
+
+// Get Quotes from API
+
+async function getQuotes() {
+  const apiUrl = "https://inspirational-quotes-api.herokuapp.com/quotes";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Tweet Quotes
+function tweetQuotes() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuotes);
+
+// On Load
+getQuotes();
