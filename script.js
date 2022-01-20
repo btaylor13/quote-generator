02b4fea1e5a61b636e3c61e4594f7eb97933b726
Bridgett Taylor <btaylor7224@gmail.com>
2022-01-19 17:16:08 -0800

    added loader while fetching quotes

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+  loading();
   // pick random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -24,13 +38,15 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  // set quote and hide the loader
   quoteText.textContent = quote.quote;
+  complete();
 }
 
 // Get Quotes from API
 
 async function getQuotes() {
+  loading();
   const apiUrl = "https://inspirational-quotes-api.herokuapp.com/quotes";
 
   try {
